542c1cd7e6d3cc72821c9bfbe81643cb788d7b3a
Kathurjan <kath097c@easv365.dk>
2022-02-28 16:11:57 +0100

    A little better ai, basically random generator

--- src/rps/bll/player/Player.java
+++ src/rps/bll/player/Player.java
@@ -7,6 +7,7 @@ import rps.bll.game.Result;
 
 //Java imports
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Example implementation of a player.
@@ -41,15 +42,31 @@ public class Player implements IPlayer {
 
     /**
      * Decides the next move for the bot...
+     *
      * @param state Contains the current game state including historic moves/results
      * @return Next move
      */
     @Override
     public Move doMove(IGameState state) {
+
         //Historic data to analyze and decide next move...
         ArrayList<Result> results = (ArrayList<Result>) state.getHistoricResults();
-
-        //Implement better AI here...
-        return Move.Rock;
+        Random random = new Random();
+        Move AiMove;
+        int result = random.nextInt(3) + 1;
+        switch (result) {
+            case 1:
+                AiMove = Move.Rock;
+                break;
+            case 2:
+                AiMove = Move.Paper;
+                break;
+            default:
+                AiMove = Move.Scissor;
+                break;
+                
+            
+        }
+        return AiMove;
     }
 }
